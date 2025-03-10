b043cea5fa7eca5d17d2d305441f897ed1665602
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-10 17:01:10 +0100

    Implementado el primer test: transcripción de Guanine a Cytosine

--- src/main/java/devanabelen/TranscriptorADN.java
+++ src/main/java/devanabelen/TranscriptorADN.java
@@ -2,4 +2,11 @@ package devanabelen;
 
 public class TranscriptorADN {
 
+    public String transcribe(String adn) {
+        // Si recibe "G", lo transcribe como "C"
+        if (adn.equals("G")) {
+            return "C";
+        }
+        return "";
+    }
 }
--- src/test/java/devanabelen/TranscriptorADNTest.java
+++ src/test/java/devanabelen/TranscriptorADNTest.java
@@ -1,5 +1,11 @@
-package devanabelen;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TranscriptorADNTest {
 
+    @Test
+    public void testTranscribeGuanineToCytosine() {
+        TranscriptorADN transcriptor = new TranscriptorADN();
+        assertEquals("C", transcriptor.transcribe("G"));
+    }
 }
